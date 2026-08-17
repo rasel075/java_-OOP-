371ed2432fb42e509c8dee21534e20d4a689f076
Rasel <harunorrashid289@gmail.com>
2026-08-17 18:34:04 +0600

    instance method

--- student.java
+++ student.java
@@ -1,7 +1,14 @@
 public class student{
     public String name; //instance variable
     public int id;
-    student(){
-        
+
+    ///Access Modifier  || returnType || Method name || (parameter name )
+    public void showDetails(){ // instance method
+        System.out.println(name+" "+id);
+
     }
+    public void standUp(){
+        System.out.println(name+" is standup ");
+    }
+  
 }
--- tester.java
+++ tester.java
@@ -1,27 +1,23 @@
 public class tester {
 
     public static void main(String[] args) {
-        student s1=new student();
-        student s2=new student();
-        s1.name ="Hasem";
-        s1.id=30;
-        s2.name="sonet";
-        s2.id=40;
-        System.out.println(s1.name+" "+s1.id);
-        System.out.println(s2.name+" "+s2.id);
-
-        s1=s2; // location s1=s2 hoye geche
-        System.out.println();
-
-        System.out.println(s1.name+" "+s1.id);
-        System.out.println(s2.name+" "+s2.id);
-
-        s1.name="Mr.perfect"; // s2.name same kortha , karon s2 er location s1 e chole geche
-        System.out.println();
-
-        System.out.println(s1.name+" "+s1.id);
-        System.out.println(s2.name+" "+s2.id);
-        
+        student s1=new student(); //object/instance1
+        student s2=new student();/// Object / instance2
+       s1.name=" Rasel";
+       s1.id=75;
+       s2.name= "Sonet";
+       s2.id= 22;
        
+       s1.showDetails();
+       s2.showDetails();
+
+       System.out.println();
+       s1=s2;
+       s1.showDetails();
+       s2.showDetails();
+
+       s1.standUp();
+
+    
     }
 }
